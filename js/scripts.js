a7478d1c2ec3f2a3004f867ca0ccab7d704f7d5a
= <=>
2023-01-08 09:34:29 -0300

    Correcion script

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+$('#info').hide();
+
 $("area").hover(function () {
     $(this).toggleClass('arriba').siblings().toggleClass('abajo')
     $('#info').show();
@@ -9,63 +11,64 @@ $("area").mouseleave(function(){
 });
 
 function pinta(region) {
-    let nombre = ''
+    //let nombre = ''
     let atrac = ''
+    const nombre = ['Arica - Parinacota','Tarapacá','Antofagasta','Atacama','Coquimbo','Valparaiso','Región Metropolitana','O Higgins','al sur','mas al sur','mucho más al sur','casi fin de mundo','cerca del fin de mundo','mas cerca del fin de mundo','ahí del fin de mundo','fin de mundo'];
     switch (region) { 
         case 0:
-            nombre = 'Arica - Parinacota';
+            //nombre = 'Arica - Parinacota';
             atrac = `<li>A</li>
             <li>B</li>
             <li>C</li>`
             break;
         case 1:
-            nombre = 'Tarapacá';
+            //nombre = 'Tarapacá';
             atrac = `<li>D</li>
             <li>E</li>
             <li>F</li>`
             break;
         case 2:
-            nombre = 'Antofagasta';
+            //nombre = 'Antofagasta';
             atrac = `<li>G</li>
             <li>H</li>
             <li>I</li>`
             break;		
         case 3:
-            nombre = 'Atacama';
+            //nombre = 'Atacama';
             atrac = `<li>J</li>
             <li>K</li>
             <li>L</li>`
             break;
         case 4:
-            nombre = 'Coquimbo';
+            //nombre = 'Coquimbo';
             atrac = `<li>M</li>
             <li>N</li>
             <li>O</li>`
             break;
         case 5:
-            nombre = 'Valparaiso';
+            //nombre = 'Valparaiso';
             atrac = `<li>P</li>
             <li>Q</li>
             <li>R</li>`
             break;
         case 6:
-            nombre = 'Región Metropolitana';
+            //nombre = 'Región Metropolitana';
             atrac = `<li>S</li>
             <li>T</li>
             <li>U</li>`
             break;		
         case 7:
-            nombre = 'O Higgins';
+            //nombre = 'O Higgins';
             atrac = `<li>V</li>
             <li>W</li>
             <li>X</li>`
             break;
         default:
-            nombre = 'Más al sur';
+            //nombre = 'Más al sur';
             atrac = `<li>Y</li>
             <li>Z</li>
             <li>FO</li>`
     }
-    $("#region").html('Region : '+nombre);
+    $("#region").html('Region : '+nombre[region]);
     $("#atractivos").html(atrac);
 }
